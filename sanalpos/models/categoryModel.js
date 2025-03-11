a671eb0dba5352795cb352228c4522e09dd14a5d
Beratsrkc <beratsarikoc6@gmail.com>
2025-03-11 21:59:32 +0300

    Cupify

--- sanalpos/models/categoryModel.js
+++ sanalpos/models/categoryModel.js
@@ -4,7 +4,7 @@ const categorySchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     subCategories: [{ type: String }]
 });
-
+    
 const CategoryModel = mongoose.models.Category || mongoose.model("Category", categorySchema);
 
 export default CategoryModel;
