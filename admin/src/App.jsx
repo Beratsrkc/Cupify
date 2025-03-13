7e4eb56817390835b4170ca828d5f3f879e0fdc9
Beratsrkc <beratsarikoc6@gmail.com>
2025-03-14 02:59:53 +0300

    İlk commit yeni repo için

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './compoments/Navbar';
-import Slidebar from './compoments/Slidebar';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import Add from './pages/Add';
-import List from './pages/List';
-import Orders from './pages/Orders';
-import Login from './compoments/Login';
+import React, { useState } from 'react'
+import Navbar from './compoments/Navbar'
+import Slidebar from './compoments/Slidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from './compoments/Login'
 import { ToastContainer } from 'react-toastify';
-import AddImage from './pages/AddImage';
-import BlogManagement from './pages/BlogManagement';
-  
+import { useEffect } from 'react'
+import AddImage from './pages/AddImage'
+import BlogManagement from './pages/BlogManagement'
+
+
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const currency = '₺';
 
@@ -26,7 +28,7 @@ const App = () => {
       {token === '' ? (
         <Login setToken={setToken} />
       ) : (
-        <Router basename="/admin">
+        <>
           <Navbar setToken={setToken} />
           <hr />
           <div className="flex w-full">
@@ -36,12 +38,12 @@ const App = () => {
                 <Route path="/add" element={<Add token={token} />} />
                 <Route path="/list" element={<List token={token} />} />
                 <Route path="/orders" element={<Orders token={token} />} />
-                <Route path="/add-image" element={<AddImage token={token} />} />
+                <Route path="/add-image" element={<AddImage token={token} />} /> 
                 <Route path="/blogs" element={<BlogManagement token={token} />} />
               </Routes>
             </div>
           </div>
-        </Router>
+        </>
       )}
     </div>
   );
