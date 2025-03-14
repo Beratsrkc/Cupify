1ccdbc3bfb72ae76a2f675430e9e1ba1ba2cb7df
Beratsrkc <beratsarikoc6@gmail.com>
2025-03-14 21:23:47 +0300

    server.js

--- sanalpos/server.js
+++ sanalpos/server.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
     }
 
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
     res.header('Access-Control-Allow-Credentials', 'true'); // Kimlik bilgilerine izin ver
     next();
 });
