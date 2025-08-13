ea2d440b1e676b502077ba732af5a332e5ea0f84
Beratsrkc <beratsarikoc6@gmail.com>
2025-08-14 02:55:52 +0300

    update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -8,12 +8,6 @@ import quality_icon from "./quality_icon.png";
 import search_icon from "./search_icon.png";
 import support_img from "./support_img.png";
 import Menu_icon from "./menu_icon.png";
-import Slider1 from "./Slider1.jpeg";
-import Slider2 from "./Slider2.jpg";
-import Slider3 from "./Slider3.jpg";
-import Slider4 from "./Slider4.jpg";
-import Slider5 from "./Slider5.jpg";
-import Slider6 from "./Slider6.jpg";
 import amex from "./amex.jpg";
 import troy from "./troy.png";
 import visa from "./visa.png";
@@ -21,21 +15,7 @@ import mastercard from "./Mastercard-logo.webp";
 import contact_img from "./contact_img.png";
 import cross_icon from "./cross_icon.png";
 import Cupify_logo_buyuk from "./logo-color.png";
-import bardakresim from "./bardakresim.png";
-import bardakresim2 from "./bardakresim2.png";
-import bardakresim3 from "./bardakresim3.png";
-import bardakresim4 from "./bardakresim4.png";
-import resim1 from "./resim1.jpg";
-import resim2 from "./resim2.jpg";
-import resim3 from "./resim3.jpg";
-import resim4 from "./resim4.png";
-import resim5 from "./resim5.png";
 import cupifybackground from "./cupify-background.png";
-import Kartonbardak2 from "./Kartonbardak2.jpg";
-import Kartonbardak from "./Kartonbardak.jpg";
-import Kartonbardak3 from "./Kartonbardak3.jpg";
-import Kartonbardak4 from "./kartonbardak4.jpg";
-import Kartonbardak5 from "./Kartonbardak5.jpg";
 import PetBardaklar from "./PetBardak.png";
 import DondurmaKasesi from "./DondurmaKasesi.png";
 import DönerBox from "./DönerBox.png";
@@ -79,21 +59,7 @@ export const assets = {
   SalataKasesi,
   DönerBox,
   Cupify_logo_buyuk,
-  Kartonbardak,
-  Kartonbardak2,
-  Kartonbardak3,
-  Kartonbardak4,
-  Kartonbardak5,
-  resim1,
-  resim2,
-  resim3,
-  resim4,
   cupifybackground,
-  resim5,
-  bardakresim2,
-  bardakresim3,
-  bardakresim4,
-  bardakresim,
   Cupify_logo,
   cart_icon,
   dropdown_icon,
@@ -104,12 +70,6 @@ export const assets = {
   bin_icon,
   support_img,
   Menu_icon,
-  Slider1,
-  Slider2,
-  Slider3,
-  Slider4,
-  Slider5,
-  Slider6,
   contact_img,
   amex,
   troy,
